d2567d5eafa2bf87b1861140789360b7ddd6fa8b
khalid <khalidobalharbi@gmail.com>
2023-12-21 02:36:31 +0300

    controller and index function and CSS

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\Models\Product;
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+
+    public function index()
+    {
+        $Product=Product::all();
+        return view('Product.index', compact('Product')); 
+    }
+
+    public function show($id)
+{
+    // $product = Product::find($id);
+    // return view('Product.show', ['product' => $product]);
+}
+
+    public function create()
+    {
+        return view('Product.create');
+    }
+    public function store(Request $request)
+    {
+        $request->validate([
+            'name' => 'required|string|max:255',
+            'description' => 'required|string',
+            'Price' => 'required|string|max:9',
+        ]);
+
+        Product::create([
+            'name' => $request->input('name'),
+            'description' => $request->input('description'),
+            'Price' => $request->input('price'),
+        ]);
+
+        return redirect()->route('Product.index')->with('success', 'Product created successfully');
+    }
+}
---
+++ resources/views/Product/index.blade.php
@@ -0,0 +1,41 @@
+<x-app-layout>
+    <x-slot name="header">
+
+    </x-slot>
+
+    <body>
+        <div class="container">
+          <div class="row">
+            <div class="col-12">
+              <div class="card">
+                    <table class="table table-bordered table-hover">
+                        <thead class="thead-dark">
+                    <tr>
+                        <th scope="col">ID</th>
+                        <th scope="col">Name</th>
+                        <th scope="col">Description</th>
+                        <th scope="col"></th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    @foreach($Product as $product)
+                    <tr lass="overdue-row">
+                        <td>{{ $product->id }}</td>
+                        <td>{{ $product->name }}</td>
+                        <td>{{ $product->description }}</td>
+                        <td>
+                        </td>
+                </tr>
+                @endforeach
+                </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
+      <a href="{{ route('Product.create')}}" class="button primary btn btn-primary"> Create New Product </a>
+    </div>
+  </body>
+        
+    
+    </div>
+</x-app-layout>
