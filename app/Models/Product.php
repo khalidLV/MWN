94eb0378de44c3f2908b478d02f0b11bb3c1e0bc
khalid <khalidobalharbi@gmail.com>
2023-12-21 02:34:31 +0300

    mod

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -7,10 +7,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
+    protected $table = 'Product';
+
     protected $fillable = [
         'name',
         'description',
-        'image'
+        'Price'
     ];
     use HasFactory;
 }
