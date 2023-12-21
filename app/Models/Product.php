b8cc07447ef59c5d8c30bec20265f04c80c081cf
khalid <khalidobalharbi@gmail.com>
2023-12-21 14:50:18 +0300

    dev - welcome page

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -1,12 +1,13 @@
 <?php
 
 namespace App\Models;
-
+use Illuminate\Notifications\Notifiable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
+    use Notifiable;
     protected $table = 'Product';
 
     protected $fillable = [
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -6,14 +6,31 @@
     </x-slot>
 
     <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+        <div class="max-w-7xl mx-auto sm:px-6">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900">
-                    {{ ("Welcome to my website!") }}
+                <div class="p-6 text-gray-900 relative">
+                    <div class="flex items-center justify-center">
+                        <p class="text-4xl font-bold mb-4">{{ __("Welcome to my website!") }}</p>
+                    </div>
+                   
+                @php
+                $Product = \App\Models\Product::all(); 
+                $productCount = $Product->count(); 
+                @endphp
+
+
+                    <span class="absolute top-0 left-0 text-black rounded-full px-2 py-1 text-l">
+                        <p class="text-4sm font-bold mb-4"> Total of Product : {{$productCount}} </p>
+
+                    </span>
+
+                    <a href="{{ route('Product.index') }}">
+                        <div class="flex items-center justify-center transition-transform transform origin-center hover:scale-105 relative">
+                            <img src="https://up6.cc/2022/02/164503212520991.jpg" alt="" />
+                        </div>
+                    </a>
                 </div>
-                <a href="{{ route('Product.index')}}" class="button primary btn btn-primary"> Start </a>
             </div>
         </div>
     </div>
 </x-app-layout>
-    
